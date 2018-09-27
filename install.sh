95273617f691e6e6a081c49f308f6ca5db5367d5
Stefan Peters <baumkronenprinz@googlemail.com>
2018-09-27 20:35:39 +0200

    added update-resources to install script

--- install.sh
+++ install.sh
@@ -9,8 +9,14 @@ make power-meter
 
 install -g dialout -m 776 power-meter /opt/power-meter
 install -g root -o root -m 744 power-meter.service /etc/systemd/system
+
+install -g root -m 776 update-resources/update-resources.py /opt/update-resources.py
+install -g root -o root -m 744 update-resources/update-resources.service /etc/systemd/system
+
 systemctl daemon-reload
 systemctl start power-meter.service
 systemctl enable power-meter.service
 
+systemctl start update-resources.service
+systemctl enable update-resources.service
 
--- update-resources/update-resources.py
+++ update-resources/update-resources.py
@@ -1,15 +1,22 @@
 #!/usr/bin/python3
 
 import re
+import time
+import sys
 import paho.mqtt.client as mqtt
 from influxdb import InfluxDBClient
 
 def on_connect(client, userdata, flags, rc):
-    assert(rc == 0)
+
+    # wait and exit (and be restarted by systemd)
+    if rc != 0:
+        time.sleep(5)
+        sys.exit(2)
+
     print("Connected!")
     client.subscribe("resources/power/#")
 
-def on_message(client, userdata, msg):
+def on_message(client, db, msg):
     #print(msg.topic, msg.payload.decode('utf-8'))
 
     json_body = [{"measurement": "power","fields": {}}]
@@ -36,6 +43,8 @@ if __name__ == "__main__":
     client = mqtt.Client(userdata=db)
     client.on_connect = on_connect
     client.on_message = on_message
+
+    # ensure we are restarted by systemd
     client.on_disconnect = lambda x: sys.exit(1)
 
     client.connect("mqtt-broker.iotnet")
